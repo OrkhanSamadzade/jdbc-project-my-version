30a4cda60fe3d8fda05819509f444daa5297b60a
Orkhan Samadzade <Orkhan.Samadzade@outlook.com>
2022-11-19 15:42:57 -0500

    like done

--- src/test/resources/Queries/day01/like.sql
+++ src/test/resources/Queries/day01/like.sql
@@ -1,1 +1,32 @@
-select * from employees;
+select * from employees
+where FIRST_NAME like 'B%';
+
+select * from employees
+where FIRST_NAME like 'B____';
+
+select * from employees
+where FIRST_NAME like '__z__';
+
+select * from employees
+where FIRST_NAME like '____e';
+
+select * from employees
+where FIRST_NAME like '%e';
+
+
+select FIRST_NAME from employees
+where FIRST_NAME like '_a%';
+
+
+select *from EMPLOYEES
+where JOB_ID like '%IT%';
+
+
+select * from EMPLOYEES
+where FIRST_NAME like 'H%l';
+
+
+
+
+
+
