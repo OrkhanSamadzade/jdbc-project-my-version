09fa65b076bf06dc89234cae64f9eefd50371296
Orkhan Samadzade <Orkhan.Samadzade@outlook.com>
2022-11-19 12:43:07 -0500

    Initial commit

---
+++ src/test/resources/Queries/day01/select.sql
@@ -0,0 +1,15 @@
+select * from employees;
+
+select *  from DEPARTMENTS;
+
+select first_name from EMPLOYEES;
+
+select  FIRST_NAME, LAST_NAME, SALARY from EMPLOYEES;
+
+select * from LOCATIONS;
+
+select CITY from LOCATIONS;
+
+select COUNTRY_NAME, REGION_ID from COUNTRIES;
+
+select JOB_ID, JOB_TITLE from JOBS;
