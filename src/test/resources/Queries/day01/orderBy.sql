515908b29fc22016ba1f089fac53d07e1c8cd289
Orkhan Samadzade <Orkhan.Samadzade@outlook.com>
2022-11-19 15:14:46 -0500

    orderBy done

---
+++ src/test/resources/Queries/day01/orderBy.sql
@@ -0,0 +1,31 @@
+select *from employees
+order by SALARY;
+
+select *from employees
+order by SALARY desc;
+
+select *from employees
+order by FIRST_NAME;
+
+select *from employees
+order by FIRST_NAME desc ;
+
+select *from employees
+order by 8;
+
+select FIRST_NAME, LAST_NAME from employees
+order by 2;
+
+select *from employees
+order by FIRST_NAME;
+
+select *from employees
+where EMPLOYEE_ID< 120
+order by SALARY desc;
+
+select * from employees
+order by FIRST_NAME, LAST_NAME desc;
+
+select * from employees
+order by  LAST_NAME desc;
+
