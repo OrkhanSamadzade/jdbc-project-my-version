b49d329223bbf95febb327f0b0dcbc7c870a6088
Orkhan Samadzade <Orkhan.Samadzade@outlook.com>
2022-11-19 12:54:25 -0500

    distinct

---
+++ src/test/resources/Queries/day01/distinct.sql
@@ -0,0 +1,19 @@
+select * from employees;
+
+-- comment
+/*
+ comment
+ */
+
+select FIRST_NAME from EMPLOYEES;
+
+
+select distinct FIRST_NAME from  EMPLOYEES;
+
+select distinct JOB_ID from JOBS;
+
+select distinct JOB_ID from EMPLOYEES;
+
+select COUNTRY_ID from LOCATIONS;
+
+select distinct COUNTRY_ID from LOCATIONS;
